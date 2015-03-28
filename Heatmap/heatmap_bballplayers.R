dd2dc49aeb2ba8fed424ee3277d8ef906cb621e6
Morgan Moskalyk <mmoskal5@uwo.ca>
2015-03-27 23:14:05 -0400

    heat map with warm colors

---
+++ Heatmap/heatmap_bballplayers.R
@@ -0,0 +1,19 @@
+#reading in data from file, make sure to change this file path
+nba <-read.csv("/Users/Moskalyk/RPlayground/ppg2008.csv", sep=",")
+
+#reordering the data by points (least to greatest)
+nba <- nba[order(nba$PTS),]
+
+#set the row name equal to the name of each player
+row.names(nba) <- nba$Name
+
+#remove the player name
+nba <- nba[,2:20]
+
+#format data into a matrix
+nba_matrix <- data.matrix(nba)
+
+#create heatmap
+nba_heatmap <- heatmap(nba_matrix, Rowv=NA, Colv=NA, scale="column", margins=c(1,1))
+
+
